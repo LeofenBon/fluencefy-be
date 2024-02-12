b17c796e50d13c225192abcabbe24c1be2f8ceef
LeofenBon <46328889+LeofenBon@users.noreply.github.com>
2024-02-13 01:40:28 +0400

    implemented authentication functionality

--- models/brand_profile.js
+++ models/brand_profile.js
@@ -1,11 +1,28 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
   class brand_profile extends Model {
     static associate(models) {
       return this.hasMany(models.project);
     }
+
+    async validatePassword(password) {
+      return await bcrypt.compare(password, this.password);
+    }
+
+    isPasswordChanged(jwtTimestamp) {
+      if (this.password_changed_at) {
+        const pChangedTimestamp = parseInt(
+          this.password_changed_at.getTime() / 1000,
+          10
+        );
+        return jwtTimestamp < pChangedTimestamp;
+      }
+      return false;
+    }
   }
+
   brand_profile.init(
     {
       id: {
@@ -17,11 +34,27 @@ module.exports = (sequelize, DataTypes) => {
       brand_name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter brand name",
+          },
+        },
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        validate: {
+          isEmail: {
+            msg: "Please enter a valid email address",
+          },
+          notNull: {
+            msg: "Please enter email address",
+          },
+        },
+        set(value) {
+          this.setDataValue("email", value.toLowerCase());
+        },
       },
       brand_picture: {
         type: DataTypes.STRING,
@@ -29,12 +62,30 @@ module.exports = (sequelize, DataTypes) => {
       password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter your password",
+          },
+          len: [8],
+        },
       },
+      password_changed_at: DataTypes.DATE,
     },
     {
       sequelize,
       modelName: "brand_profile",
     }
   );
+
+  brand_profile.beforeCreate(async (brand, options) => {
+    try {
+      const hashedPassword = await bcrypt.hash(brand.password, 12);
+      console.log("hashedPassword", hashedPassword);
+      brand.password = hashedPassword;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
   return brand_profile;
 };
--- models/influencer_profile.js
+++ models/influencer_profile.js
@@ -1,9 +1,26 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
   class influencer_profile extends Model {
     static associate(models) {
       this.belongsToMany(models.project, { through: models.proposal });
+    }
+    async validatePassword(password) {
+      return await bcrypt.compare(password, this.password);
+    }
+
+    isPasswordChanged(jwtTimestamp) {
+      if (this.password_changed_at) {
+        const pChangedTimestamp = parseInt(
+          this.password_changed_at.getTime() / 1000,
+          10
+        );
+        console.log("jwtTimestamp", jwtTimestamp);
+        console.log("pChangedTimestamp", pChangedTimestamp);
+        return jwtTimestamp < pChangedTimestamp;
+      }
+      return false;
     }
   }
   influencer_profile.init(
@@ -17,19 +34,43 @@ module.exports = (sequelize, DataTypes) => {
       first_name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter your first name",
+          },
+        },
       },
       last_name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter your last name",
+          },
+        },
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        validate: {
+          isEmail: {
+            msg: "Please enter a valid email address",
+          },
+          notNull: {
+            msg: "Please enter your email address",
+          },
+        },
       },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter your password",
+          },
+          len: [8],
+        },
       },
       niche: {
         type: DataTypes.STRING,
@@ -46,11 +87,23 @@ module.exports = (sequelize, DataTypes) => {
       twitter_link: {
         type: DataTypes.STRING,
       },
+      password_changed_at: DataTypes.DATE,
     },
     {
       sequelize,
       modelName: "influencer_profile",
     }
   );
+
+  influencer_profile.beforeCreate(async (influencer, options) => {
+    try {
+      const hashedPassword = await bcrypt.hash(influencer.password, 12);
+      influencer.password = hashedPassword;
+    } catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+  });
+
   return influencer_profile;
 };
